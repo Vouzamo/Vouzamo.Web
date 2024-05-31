481c1323415945e43d6a23231be6c1e988e08fe2
John Askew <john.askew@vouzamo.com>
2024-05-31 13:35:28 -0500

    first commit

---
+++ src/Vouzamo.Web.WebApp/Pages/Pool.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vouzamo.Web.WebApp.Pages
+{
+    public class PoolModel : PageModel
+    {
+        private readonly ILogger<PoolModel> _logger;
+
+        public PoolModel(ILogger<PoolModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
